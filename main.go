b106ca5eaa7cdc5d7254d90ae0172cbf27cbe58f
Kerynn Davis <kerynn.davis@gmail.com>
2024-07-11 15:12:07 -0700

    add inputoutput and command managers

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 
-	"example.com/price-calculator/filemanager"
+	// "example.com/price-calculator/filemanager"
 	"example.com/price-calculator/prices"
+	"example.com/price-calculator/cmdmanager"
 )
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
 		priceJob.Process()
 	}
 }
