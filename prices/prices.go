f5d3dda62dcbdd8e530541112fc2f2d79de04328
Kerynn Davis <kerynn.davis@gmail.com>
2024-07-11 14:39:36 -0700

    create filemanager struct for more generic path

--- prices/prices.go
+++ prices/prices.go
@@ -7,6 +7,7 @@ import (
 )
 
 type TaxIncludedPriceJob struct {
+	IOManager filemanager.FileManager
 	TaxRate float64
 	InputPrices []float64
 	TaxIncludedPrices map[string]string
@@ -14,7 +15,7 @@ type TaxIncludedPriceJob struct {
 
 func (job *TaxIncludedPriceJob) LoadData() {
 
-	lines, err := filemanager.ReadLines("prices.txt")
+	lines, err := job.IOManager.ReadLines()
 
 	if err != nil {
 		fmt.Println(err)
@@ -43,11 +44,12 @@ func (job *TaxIncludedPriceJob) Process() {
 
 	job.TaxIncludedPrices = result
 
-	filemanager.WriteJSON(fmt.Sprintf("result_%.0fjson", job.TaxRate*100), job)
+	job.IOManager.WriteResult(job)
 }
 
-func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(fm filemanager.FileManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
+		IOManager: fm,
 		InputPrices: []float64{10, 20, 30},
 		TaxRate: taxRate,
 	}
